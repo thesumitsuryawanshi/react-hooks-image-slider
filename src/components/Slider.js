eed8cbc4b7494a7d93026734e29537768066c6c5
Dan <danielzuzevich@gmail.com>
2020-03-01 10:45:22 -0500

    removed old unused prop on Slide

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -102,7 +102,7 @@ const Slider = props => {
         width={getWidth() * _slides.length}
       >
         {_slides.map((_slide, i) => (
-          <Slide width={getWidth()} key={_slide + i} content={_slide} images />
+          <Slide width={getWidth()} key={_slide + i} content={_slide} />
         ))}
       </SliderContent>
 
