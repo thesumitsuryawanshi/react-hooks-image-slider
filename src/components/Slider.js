18b04245c51772bda975f50203c8e20f81677e52
Nicola Gaioni <nicog.designer@gmail.com>
2021-06-25 02:45:24 +0200

    reset timeout when active slide changes

--- src/components/Slider.js
+++ src/components/Slider.js
@@ -41,10 +41,6 @@ const Slider = props => {
   useEffect(() => {
     const slider = sliderRef.current
 
-    const play = () => {
-      autoPlayRef.current()
-    }
-
     const smooth = e => {
       if (e.target.className.includes('SliderContent')) {
         transitionRef.current()
@@ -58,21 +54,31 @@ const Slider = props => {
     const transitionEnd = slider.addEventListener('transitionend', smooth)
     const onResize = window.addEventListener('resize', resize)
 
-    let interval = null
-
-    if (props.autoPlay) {
-      interval = setInterval(play, props.autoPlay * 1000)
-    }
-
     return () => {
       slider.removeEventListener('transitionend', transitionEnd)
       window.removeEventListener('resize', onResize)
 
-      if (props.autoPlay) {
-        clearInterval(interval)
-      }
+    
     }
   }, [])
+
+  useEffect(() => {
+    const play = () => {
+        autoPlayRef.current();
+    }
+  
+    let interval = null
+    
+    if (props.autoPlay) {
+        interval = setInterval(play, props.autoPlay * 1000);
+    }
+    
+    return () => {
+        if (props.autoPlay) {
+            clearInterval(interval);
+        }
+    };
+  }, [activeSlide])
 
   useEffect(() => {
     if (transition === 0) setState({ ...state, transition: 0.45 })
